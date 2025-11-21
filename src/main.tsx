465cdc57569ed87844d6b29cb98d4357e46da2fb
Lloydgapton <lloydgapton123@gmail.com>
2025-11-21 13:31:15 -0800

    implemented header component with appropriate stylings

--- src/main.tsx
+++ src/main.tsx
@@ -5,9 +5,11 @@ import './index.css'
 import Button from './components/common/Button'
 import HomePage from './pages/Homepage';
 import CharityPage from './pages/CharityPage';
+import Header from './components/common/header';
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <BrowserRouter>
+    <Header />
     <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/charity" element={<CharityPage />} /> 
