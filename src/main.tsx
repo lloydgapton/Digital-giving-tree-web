87d69536314efdff1385c300a7d93f5096349d31
Lloydgapton <lloydgapton123@gmail.com>
2025-11-21 13:54:09 -0800

    installed and implemented swipper at the Hero section

--- src/main.tsx
+++ src/main.tsx
@@ -6,10 +6,13 @@ import Button from './components/common/Button'
 import HomePage from './pages/Homepage';
 import CharityPage from './pages/CharityPage';
 import Header from './components/common/header';
+import HeroCarousel from './components/common/hero-carousel';
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <BrowserRouter>
     <Header />
+    <HeroCarousel />
     <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/charity" element={<CharityPage />} /> 
