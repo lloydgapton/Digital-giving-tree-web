42ec2c3b82821b2bed832209b36f7a731de05cf5
Lloydgapton <lloydgapton123@gmail.com>
2025-11-21 12:48:11 -0800

    installed the tailwind-merge package

--- src/main.tsx
+++ src/main.tsx
@@ -1,17 +1,18 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { BrowserRouter } from 'react-router-dom'
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import './index.css'
 import Button from './components/common/Button'
-import Homepage from './pages/Homepage';
+import HomePage from './pages/Homepage';
 import CharityPage from './pages/CharityPage';
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <BrowserRouter>
-      <Button />
-      <Homepage />
-      <CharityPage />
+    <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/charity" element={<CharityPage />} /> 
+    </Routes>
+    <Button />
     </BrowserRouter>
   </StrictMode>,
 )
-
