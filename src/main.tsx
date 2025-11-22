57a469c5167233fd8778a66925477d862323b2cd
Lloydgapton <lloydgapton123@gmail.com>
2025-11-22 09:33:46 -0800

    removed button

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,6 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import './index.css';
-import { Button } from './components/common/Button';
 import HomePage from './pages/Homepage';
 import CharityPage from './pages/CharityPage';
 import Header from './components/common/header';
@@ -51,7 +50,6 @@ createRoot(document.getElementById('root')!).render(
         <Route path="/" element={<HomePage />} />
         <Route path="/charity" element={<CharityPage />} />
       </Routes>
-      <Button />
     </BrowserRouter>
   </StrictMode>
 );
